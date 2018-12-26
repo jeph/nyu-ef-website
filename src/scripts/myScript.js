c3a34605f8587d43caee85bea4bae66787613e43
Deanna <dt1543@nyu.edu>
2018-12-26 04:15:23 -0500

    include line scroll

--- src/scripts/myScript.js
+++ src/scripts/myScript.js
@@ -1,4 +1,7 @@
+
 $(document).ready(function(){
+  lineScroll();
+  
   $window.on('scroll', navShadow);
   $window.on('scroll resize', check_if_in_view);
   $window.trigger('scroll');
@@ -42,4 +45,29 @@ function navShadow(){
     $("#navbar-container").css("box-shadow", "none");
   }
 
+}
+
+
+function lineScroll(){
+  // Get the id of the <path> element and the length of <path>
+  const line = document.getElementById("line");
+  const length = line.getTotalLength();
+
+  // The start position of the drawing
+  line.style.strokeDasharray = length;
+
+  // Hide the triangle by offsetting dash. Remove this line to show the triangle before scroll draw
+  line.style.strokeDashoffset = length;
+
+  // Find scroll percentage on scroll (using cross-browser properties), and offset dash same amount as percentage scrolled
+  window.addEventListener("scroll", myFunction);
+
+  function myFunction() {
+    let scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+
+    let draw = length * scrollpercent;
+
+    // Reverse the drawing (when scrolling upwards)
+    line.style.strokeDashoffset = length - draw;
+  }
 }
