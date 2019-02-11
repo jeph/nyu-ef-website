b5a24f60f944181eefc9b9edb44ff27bd809ebb5
Jephrey Liu <jephliu@jeph.io>
2019-02-11 18:14:31 -0500

    Refactos

--- src/scripts/w3Script.js
+++ src/scripts/w3Script.js
@@ -1,9 +1,8 @@
-
 function toggleFunction() {
-    var x = document.getElementById("navbar");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
-    }
+  const x = document.getElementById("navbar");
+  if (x.className.indexOf("w3-show") === -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
 }
