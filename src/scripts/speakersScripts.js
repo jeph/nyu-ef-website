93ed91d8cfb46507a90ee074e03228bc5e8e9ff8
rebecca112233 <rebecca.shi@nyu.edu>
2019-03-05 11:58:13 -0500

    updated speakers page with links and descriptions

--- src/scripts/speakersScripts.js
+++ src/scripts/speakersScripts.js
@@ -3,3 +3,15 @@ document.getElementById('panels-section').onclick=function(){
 };
 
 document.getElementById('panels-section').style.cursor="pointer";
+
+document.getElementById('roundtables-section').onclick=function(){
+    location.href="roundtables.html";
+};
+
+document.getElementById('roundtables-section').style.cursor="pointer";
+
+document.getElementById('workshops-section').onclick=function(){
+    location.href="workshops.html";
+};
+
+document.getElementById('workshops-section').style.cursor="pointer";
